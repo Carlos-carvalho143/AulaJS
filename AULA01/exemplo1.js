3555546d63e3a3b3f626e28477ec9e1ebf610abe
Carlos-carvalho143 <carvalhomariaeduarda27@gmail.com>
2026-02-24 11:36:59 -0300

    24/02/26

---
+++ AULA01/exemplo1.js
@@ -0,0 +1,13 @@
+document.write("<h2> entrada de dados </h2>");
+
+var Numero1 = prompt("digite o numero 1");
+var Numero2 = prompt("digite o numero 2");
+
+document.write("numero 1 = " + Numero1 + "<br>");
+document.write("numero 2 = " + Numero2 + "<br>");
+
+document.write("<h2>operações aritmeticas</h2>");
+var resultado;
+
+resultado = Number(Numero1) + Number(Numero2);
+document.write("a soma dos numeros é " + resultado + "<br>");
---
+++ AULA01/script.js
@@ -0,0 +1,14 @@
+var nome = "carlos eduardo"
+var idade = "15"
+var altura = "1,80"
+
+console.log( nome );
+console.log( idade );
+console.log( altura );
+
+document.write( "<h1> olá seja bem vindo ao javascript <br>" )
+document.write( nome + "<br>" );
+document.write( idade + "<br>");  
+document.write( altura + "<br>");
+var dias = idade * 365
+document.write("<p style=' color : red'> voce viveu " + dias +" dias </p>")
